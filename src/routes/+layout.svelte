90f30153c3a929737b7d8aba2f0c1b01142579a6
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-01-31 20:10:00 +1100

    Add placeholder home and documents page route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,19 @@
 	import { page } from '$app/state';
 	import { locales, localizeHref } from '$lib/paraglide/runtime';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Header, Footer } from '$lib/components';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-{@render children()}
-<div style="display:none">
-	{#each locales as locale}
-		<a href={localizeHref(page.url.pathname, { locale })}>
-			{locale}
-		</a>
-	{/each}
-</div>
+<header></header>
+
+<Header />
+
+<main>
+	{@render children()}
+</main>
+
+<Footer />
