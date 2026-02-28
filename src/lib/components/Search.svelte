6750b772b911d3ed5db3d07aafe68be2cfe2a621
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-02-28 12:33:46 +1100

    Add scaffold for <Search> component

---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  interface Props {
+    label?: string;
+    query?: string;
+    onKeyUp: (event: KeyboardEvent) => void;
+  }
+
+  let { label, query, onKeyUp }: Props = $props();
+</script>
+
+<div class="pills">
+  {#if label}
+    <p>{label}</p>
+  {/if}
+
+  <input type="search" value={query} onkeyup={(e) => onKeyUp(e)} />
+</div>
