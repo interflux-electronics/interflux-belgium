309ee341984308157539ce3b7fda57d338e9c2e1
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-02-28 12:33:19 +1100

    Add <Pills> component

---
+++ src/lib/components/Pills.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { Button } from '$lib/components';
+
+  interface Option {
+    label: string;
+    value?: string;
+  }
+
+  interface Props {
+    label?: string;
+    options: Option[];
+    selected?: Option;
+    onSelect: (option: Option | undefined) => void;
+  }
+
+  let { label, options, selected, onSelect }: Props = $props();
+
+  function select(option: Option | undefined) {
+    onSelect(option);
+  }
+
+  function reset() {
+    select(undefined);
+  }
+</script>
+
+<div class="pills">
+  {#if label}
+    <p>{label}</p>
+  {/if}
+
+  {#if selected}
+    <Button label={selected.label} onclick={() => reset()} />
+  {:else}
+    {#each options as option}
+      <Button label={option.label} onclick={() => select(option)} />
+    {/each}
+  {/if}
+</div>
