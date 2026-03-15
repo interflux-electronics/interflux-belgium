dc553559717e858e2cd946826a5283fc96f4d064
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-03-15 12:18:53 +1100

    Dry up the document server load

--- src/routes/documents/+page.server.ts
+++ src/routes/documents/+page.server.ts
@@ -1,31 +1,34 @@
-import { PUBLIC_API_HOST } from '$env/static/public';
+import { PUBLIC_API_HOST as api } from '$env/static/public';
 import { normalizeJsonApi } from '$lib/api/normalize';
+import { getOptions } from '$lib/state/headers.svelte';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
+  const getJson = (response: Response) => {
+    if (response.status !== 200) {
+      throw new Error('Fetch failed');
+    }
+    return response.json();
+  };
+
+  const normalize = (json: any) => {
+    return normalizeJsonApi(json);
+  };
+
   const [documents, products] = await Promise.all([
-    fetch(`${PUBLIC_API_HOST}/v1/public/documents`)
-      .then((r) => {
-        if (r.status !== 200) throw new Error('Document fetch failed');
-        return r.json();
-      })
-      .then((json) => {
-        return normalizeJsonApi(json);
-      }),
-      
-    fetch(`${PUBLIC_API_HOST}/v1/public/products`)
-      .then((r) => {
-        if (r.status !== 200) throw new Error('Product fetch failed');
-        return r.json();
-      })
-      .then((json) => {
-        return normalizeJsonApi(json);
-      })
+    fetch(`${api}/v1/public/documents`, getOptions).then(getJson).then(normalize),
+    fetch(`${api}/v1/public/products`, getOptions).then(getJson).then(normalize)
   ]);
+
+  // TODO?
+  // const [documents, products] = await Promise.all([
+  //   findAll('documents'),
+  //   findAll('products')
+  // ]);
 
   return {
     documents,
     products,
-    theme: 'blue overlap'
+    theme: 'blue overlap' // configured per route
   };
 };
