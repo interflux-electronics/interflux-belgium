fb3332781c6716850f0cd4b61bdb7e865bb17b7c
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-05-23 17:06:55 +1000

    Improve deploy script

--- bin/deploy-remote.sh
+++ bin/deploy-remote.sh
@@ -2,23 +2,38 @@
 
 set -eou pipefail
 
+echo "----------"
+echo "⛵️ deploying ..."
+echo "----------"
+
 cd /var/www/prototype.interflux.com
 sudo -u interflux bash -lc "git pull"
 
 echo "----------"
-echo "✅ Pulled latest production code"
+echo "✅ git pull"
+echo "----------"
+
+sudo -u interflux bash -lc "npm install"
+
+echo "----------"
+echo "✅ nvm install"
+echo "----------"
+
+sudo -u interflux bash -lc "npm install"
+
+echo "----------"
+echo "✅ npm install"
 echo "----------"
 
 sudo -u interflux bash -lc "npm run build"
 
 echo "----------"
-echo "✅ Build complete"
+echo "✅ npm run build"
 echo "----------"
 
 sudo systemctl restart prototype.interflux.com
-
 sudo systemctl status prototype.interflux.com --no-pager
 
 echo "----------"
-echo "✅ Daemon restarted"
+echo "✅ systemctl restart prototype.interflux.com"
 echo "----------"
