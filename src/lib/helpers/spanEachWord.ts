72db511b3b37320694feaa3b29bdfd07a2b2f32b
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-06-14 06:59:40 +1000

    Remove comment

--- src/lib/helpers/spanEachWord.ts
+++ src/lib/helpers/spanEachWord.ts
@@ -10,24 +10,3 @@ export function spanEachWord(phrase: string) {
 
   return html;
 }
-
-// let textWithWordSpans = $derived.by(() => {
-//   if (!label) {
-//     console.warn('no @text passed into <Tag>');
-//     return null;
-//   }
-
-//   if (typeof label !== 'string') {
-//     console.warn('no valid string passed into <Tag>');
-//     console.warn(label);
-//     return null;
-//   }
-
-//   const words = label.split(' ');
-
-//   const html = `<nobr><span class="word">${words.join(
-//     '</span><span class="space">&nbsp;</span></nobr><nobr><span class="word">'
-//   )}</span></nobr>`;
-
-//   return html;
-// });
