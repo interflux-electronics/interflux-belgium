c47f9a2eb5484d80c658f50673da28709028b23e
Jan Johannes Werkhoven 岩山 <jan.werkhoven@gmail.com>
2026-07-01 16:51:47 +1000

    Rename $app/page back to page instead of app

--- src/lib/components/DesktopHeader.svelte
+++ src/lib/components/DesktopHeader.svelte
@@ -4,7 +4,7 @@
   import { m } from '$lib/paraglide/messages';
   import { markdown } from '$lib/helpers';
   import { header } from '$lib/state/header.svelte';
-  import { page as app } from '$app/state';
+  import { page } from '$app/state';
 
   function closeMenu() {
     header.shownMenu = 'none';
@@ -38,7 +38,7 @@
   }
 </script>
 
-<header id="desktop" class={app.data.theme}>
+<header id="desktop" class={page.data.headerTheme}>
   <div class="bg"></div>
 
   <nav>
